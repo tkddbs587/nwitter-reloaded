fcedbdb8a3af072656c44d2043ec755a18e3cad6
hoon <dudrn587@gmail.com>
2025-01-23 22:25:18 +0900

    feat: 트윗 텍스트 내용 수정 기능 추가

--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 import { ITweet } from "./timeline";
 import { auth, db, storage } from "../firebase";
-import { deleteDoc, doc } from "firebase/firestore";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
 import { deleteObject, ref } from "firebase/storage";
+import { useState } from "react";
 
 const Wrapper = styled.div`
   display: grid;
@@ -13,6 +14,8 @@ const Wrapper = styled.div`
 `;
 
 const Column = styled.div`
+  display: flex;
+  flex-direction: column;
   &:last-child {
     place-self: end;
   }
@@ -34,8 +37,18 @@ const Payload = styled.p`
   font-size: 18px;
 `;
 
-const DeleteButton = styled.button`
-  background-color: tomato;
+const EditForm = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const EditInput = styled.textarea`
+  padding: 8px;
+  resize: none;
+`;
+
+const EditButton = styled.button`
+  background-color: skyblue;
   color: white;
   font-weight: 600;
   border: 0;
@@ -44,24 +57,74 @@ const DeleteButton = styled.button`
   text-transform: uppercase;
   border-radius: 5px;
   cursor: pointer;
+  width: 15%;
+`;
+
+const EditConfirmButton = styled.button`
+  background-color: skyblue;
+  color: white;
+  font-weight: 600;
+  border: 0;
+  font-size: 12px;
+  padding: 5px 10px;
+  text-transform: uppercase;
+  border-radius: 5px;
+  cursor: pointer;
+  width: 15%;
+`;
+
+const DeleteButton = styled.button`
+  background-color: tomato;
+  color: white;
+  font-weight: 600;
+  border: 0;
+  font-size: 12px;
+  padding: 5px 10px;
+  margin-top: 4px;
+  text-transform: uppercase;
+  border-radius: 5px;
+  cursor: pointer;
+  width: 15%;
 `;
 
 function Tweet({ username, photo, tweet, userId, id }: ITweet) {
+  const [isLoding, setIsLoding] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editInputValue, setEditInputValue] = useState(tweet);
+  const tweetDocRef = doc(db, "tweets", id);
   const user = auth.currentUser;
+
+  const onEditConfirm = () => {
+    if (user?.uid !== userId) return;
+    try {
+      setIsLoding(true);
+      if (!user || isLoding || tweet === "" || tweet.length > 180) return;
+      if (tweet === editInputValue) {
+        setIsEditing(false);
+        return;
+      }
+      updateDoc(tweetDocRef, {
+        tweet: editInputValue,
+      });
+      setIsEditing(false);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setIsLoding(false);
+    }
+  };
 
   const onDelete = async () => {
     const ok = confirm("Are you sure you want to delete this tweet?");
     if (!ok || user?.uid !== userId) return;
     try {
-      await deleteDoc(doc(db, "tweets", id));
+      await deleteDoc(tweetDocRef);
       if (photo) {
         const photoRef = ref(storage, `tweets/${user.uid}/${id}`);
         await deleteObject(photoRef);
       }
     } catch (e) {
       console.log(e);
-    } finally {
-      //
     }
   };
 
@@ -69,9 +132,24 @@ function Tweet({ username, photo, tweet, userId, id }: ITweet) {
     <Wrapper>
       <Column>
         <Username>{username}</Username>
-        <Payload>{tweet}</Payload>
+        {isEditing ? null : <Payload>{tweet}</Payload>}
         {user?.uid === userId ? (
-          <DeleteButton onClick={onDelete}>Delete</DeleteButton>
+          <>
+            {isEditing ? (
+              <EditForm>
+                <EditInput
+                  onChange={(e) => setEditInputValue(e.target.value)}
+                  value={editInputValue}
+                />
+                <EditConfirmButton onClick={onEditConfirm}>
+                  수정
+                </EditConfirmButton>
+              </EditForm>
+            ) : (
+              <EditButton onClick={() => setIsEditing(true)}>Edit</EditButton>
+            )}
+            <DeleteButton onClick={onDelete}>Delete</DeleteButton>
+          </>
         ) : null}
       </Column>
       <Column>{photo ? <Photo src={photo} /> : null}</Column>
